8952a5ff2a811a2c979ab23f6d5a1f88ee04973d
Mateus Leandro <132153413+LSaints@users.noreply.github.com>
2024-09-07 18:21:20 +0000

    Adicionar func para retorna o nome das roles

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -6,4 +6,12 @@ enum Role: int
 {
     case Teacher = 1;
     case Student = 2;
+
+    public function name(): string
+    {
+        return match ($this) {
+            self::Teacher => 'Professor',
+            self::Student => 'Aluno',
+        };
+    }
 }
